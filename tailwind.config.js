285ec403ea2cd0bbaa9a180c5cc70d4715f7876e
hmnshGupta <himanshucool175@gmail.com>
2024-05-18 09:16:01 +0530

    config the tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/main/resources/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
+
